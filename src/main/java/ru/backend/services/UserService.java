d870ccce5c70ff3a2d58087858647341c46c8cde
vudovenko <vladislav.udovenko0@yandex.ru>
2024-04-28 14:21:33 +0500

    Вывод юзеров через jdbc

--- src/main/java/ru/backend/services/UserService.java
+++ src/main/java/ru/backend/services/UserService.java
@@ -6,15 +6,16 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.backend.entities.User;
-import ru.backend.enums.Role;
 import ru.backend.enums.UserStatus;
 import ru.backend.repositories.UserRepository;
 import ru.backend.security.auth.UserDTO;
 import ru.backend.security.user.Role;
+import ru.backend.security.user.SecurityUser;
 
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @Service
 @AllArgsConstructor
@@ -23,9 +24,6 @@ public class UserService {
     private final JdbcTemplate jdbcTemplate;
     private final UserRepository securityUserRepository;
 
-    @Autowired
-    public UserService(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
     public void saveOrUpdateUser(UserDTO userDTO) {
 
 //        securityUserRepository.(userDTO);
@@ -35,17 +33,18 @@ public class UserService {
         List<User> users = new ArrayList<>();
         jdbcTemplate.query("SELECT * FROM users WHERE role = 'USER'", (ResultSet rs, int rowNum) -> {
             User user = new User();
+            SecurityUser securityUser = new SecurityUser();
+            user.setSecurityUser(securityUser);
+            securityUser.setSquadUser(user);
             user.setId(rs.getInt("id"));
-            user.setLogin(rs.getString("login"));
-            user.setPassword(rs.getString("password"));
             user.setName(rs.getString("name"));
             user.setSurname(rs.getString("surname"));
             user.setJobTitle(rs.getString("job_title"));
             user.setContacts(rs.getString("contacts"));
-            user.setStatus(rs.getString("status").equals("ACTIVE")
-                    ? UserStatus.ACTIVE
-                    : UserStatus.DELETED);
-            user.setRole(Role.USER);
+            securityUser.setPassword(rs.getString("password"));
+            securityUser.setLogin(rs.getString("login"));
+            securityUser.setActive(rs.getString("status").equals("ACTIVE"));
+            securityUser.setRoles(Set.of(Role.USER));
             users.add(user);
             return user;
         });
