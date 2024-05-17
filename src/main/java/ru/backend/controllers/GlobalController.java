0b6b66de908df9d139c340b9a1d60d27ef4fb7c8
Andrey <dikovadnrey856@gmail.com>
2024-05-17 05:55:55 +0500

    добавил необходимые todo для оставшихся работ

--- src/main/java/ru/backend/controllers/GlobalController.java
+++ src/main/java/ru/backend/controllers/GlobalController.java
@@ -9,6 +9,10 @@ import ru.backend.security.user.SecurityUser;
 @ControllerAdvice
 public class GlobalController {
 
+    //todo: сделать обновление токена при обновлении логина
+    //todo: сделать добавление/обновление задач и распределение их по статусам
+    //todo: сделать валидацию для всех форм
+
     @ModelAttribute("currentUser")
     public SecurityUser getCurrentUser(@AuthenticationPrincipal UserDetails currentUser) {
         return (SecurityUser)currentUser;
